6adfd1ff760c8d22ff54cc2362482267ebc8a786
Saad Satter <Saad.Satter@stonybrook.edu>
2020-10-08 19:31:07 -0400

    Added methods for BookCopy class

--- Library/BookCopy.cpp
+++ Library/BookCopy.cpp
@@ -1,2 +1,101 @@
 #include "BookCopy.h"
 //hi
+BookCopy::BookCopy()
+{
+	ISBN = "";
+	Title = "";
+	Author = "";
+	Category = "";
+	ID = "";
+	readerName = "";
+	start_date = "";
+	res_date = "";
+	exp_date = "";
+}
+
+BookCopy::BookCopy(std::string isbn, std::string title, std::string author, std::string category, std::string id, std::string reader_name,
+	std::string startDate, std::string resDate, std::string expDate)
+{
+	ISBN = isbn;
+	Title = title;
+	Author = author;
+	Category = category;
+	ID = id;
+	readerName = reader_name;
+	start_date = startDate;
+	res_date = resDate;
+	exp_date = expDate;
+}
+
+std::string BookCopy::getISBN()
+{
+	return ISBN;
+}
+std::string BookCopy::getTitle()
+{
+	return Title;
+}
+std::string BookCopy::getAuthor()
+{
+	return Author;
+}
+std::string BookCopy::getCategory()
+{
+	return Category;
+}
+std::string BookCopy::getID()
+{
+	return ID;
+}
+std::string BookCopy::getReaderName()
+{
+	return readerName;
+}
+std::string BookCopy::get_start_date()
+{
+	return start_date;
+}
+std::string BookCopy::get_res_date()
+{
+	return res_date;
+}
+std::string BookCopy::get_exp_date()
+{
+	return exp_date;
+}
+void BookCopy::setISBN(std::string isbn)
+{
+	ISBN = isbn;
+}
+void BookCopy::setTitle(std::string title)
+{
+	Title = title;
+}
+void BookCopy::setAuthor(std::string author)
+{
+	Author = author;
+}
+void BookCopy::setCategory(std::string category)
+{
+	Category = category;
+}
+void BookCopy::setID(std::string id)
+{
+	ID = id;
+}
+void BookCopy::setReaderName(std::string reader_name)
+{
+	readerName = reader_name;
+}
+void BookCopy::set_start_date(std::string startDate)
+{
+	start_date = startDate;
+}
+void BookCopy::set_exp_date(std::string startDate)
+{
+	exp_date = expDate;
+}
+void BookCopy::set_res_date(std::string startDate)
+{
+	res_date = resDate;
+}
--- Library/BookCopy.h
+++ Library/BookCopy.h
@@ -1,25 +1,38 @@
 #pragma once
+
 class BookCopy
 {
 private:
 	std::string ISBN;
 	std::string Title;
 	std::string Author;
-	std::string Category
-	std::string ID
-	std::string readerName
-	auto start_date;
-	auto res_date;
-	auto exp_date;
-pubic:
+	std::string Category;
+	std::string ID;
+	std::string readerName;
+	std::string start_date;
+	std::string res_date;
+	std::string exp_date;
+public:
 	BookCopy();
+	BookCopy(std::string isbn, std::string title, std::string author, std::string category, std::string id, std::string reader_name,
+		std::string startDate, std::string resDate, std::string expDate);
 	std::string getISBN();
 	std::string getTitle();
 	std::string getAuthor();
 	std::string getCategory();
 	std::string getID();
 	std::string getReaderName();
-	auto get_start_date();
-	    
+	std::string get_start_date();
+	std::string get_res_date();
+	std::string get_exp_date();
+	void setISBN(std::string isbn);
+	void setTitle(std::string title);
+	void setAuthor(std::string author);
+	void setCategory(std::string category);
+	void setID(std::string id);
+	void setReaderName(std::string reader_name);
+	void set_start_date(std::string startDate);
+	void set_res_date(std::string resDate);
+	void set_exp_date(std::string expDate);
 };
 
